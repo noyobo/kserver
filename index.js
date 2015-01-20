a80e1598b0b756b692785ff2cc30ee9e7ba395ce
noyobo <nongyoubao@alibaba-inc.com>
2015-01-20 11:05:41 +0800

    jshint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
+/*jshint camelcase:false*/
 var child_process = require('child_process');
 var path = require('path');
 var server = null;
 
 module.exports = function(config) {
+  /*jshint camelcase:false*/
   server = child_process.fork(path.resolve(__dirname, './lib/server.js'));
   server.send({
     cmd: 'start',
@@ -16,13 +18,15 @@ module.exports = function(config) {
 process.on('uncaughtException', function(err) {
   if (!server) {
     throw err;
-  };
+  }
+  /*jshint -W030*/
   server && server.send({
     cmd: 'exit'
   });
 });
-
+/*jshint unused:false*/
 process.on('exit', function(code) {
+  /*jshint -W030*/
   server && server.send({
     cmd: 'exit'
   });
